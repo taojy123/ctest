3e0a9b518616a9fc86b90a72dd3fffdd823ade4e
taojy123 <taojy123@163.com>
2020-04-03 13:11:18 +0800

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 mkdir -p  /var/data;
 
 if [[ -z "$1" ]]; then
+    bash
+elif [[ "$1" = "flower" ]]; then
     echo "run celery worker";
-    celery worker -A app -l info;
+    celery worker -A app;
 elif [[ "$1" = "flower" ]]; then
     echo "run celery flower";
     flower -A app --address=0.0.0.0 --db=/var/data/flower.db --persistent=True;
