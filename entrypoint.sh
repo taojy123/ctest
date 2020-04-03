98c0ad4c564eec6cc5cd591344267c86ed533b1b
taojy123 <taojy123@163.com>
2020-04-03 17:00:05 +0800

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [[ -z "$1" ]]; then
     bash
 elif [[ "$1" = "worker" ]]; then
     echo "run celery worker";
-    celery worker -A app -l debug;
+    celery worker -A app -l info;
 elif [[ "$1" = "flower" ]]; then
     echo "run celery flower";
     flower -A app --address=0.0.0.0 --db=/var/data/flower.db --persistent=True;
